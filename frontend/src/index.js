9fa7cf9ee7992df2ac3c0f99c09a79694f969a46
Daniel Barben <daniel.barben@tamedia.ch>
2019-04-20 00:20:13 +0200

    Hashrouter added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import Header from './Landingpage/Header.js';
 import Footer from './Landingpage/Footer.js';
@@ -8,7 +8,7 @@ import App from './App';
 import Landingpage from './Landingpage';
 
 ReactDOM.render(
-  <BrowserRouter >
+  <HashRouter >
   <div>
     <Header/>
     <Switch>
@@ -17,5 +17,5 @@ ReactDOM.render(
     </Switch>
     <Footer/>
     </div>
-  </BrowserRouter>
+  </HashRouter>
 , document.getElementById('root'));
