e77183920c8c8799da97fea09760f2c358658c7d
Daniel Barben <daniel.barben@tamedia.ch>
2019-04-25 20:08:30 +0200

    new Endpoint

--- backend/api/controllers.js
+++ backend/api/controllers.js
@@ -1,46 +1,50 @@
 const db = require('../db/index.js');
 
 const homeRoute = (req, res) => {
-    res.send("Server is running")
+    res.status(200).send("Das Orakel denkt nach!")
 }
 const landingpage = (req, res) => {
     db.models.Projects
-    //.findAll({ order: [['order', 'ASC']]})
     .findAll({
-        where: {status : 'online'},
+        where: {
+            status : 'online'
+        },
         order: [['order', 'ASC']]
     })
     .then(projects => {
-        //projects.findAll({ where: [oder != 0]})
         res.status(200).send(projects);
     })
 };
 
 const findAllProjects = (req, res) => {
     db.models.Projects
-    .findAll({include: [{
-        model: db.models.Questions, include: [db.models.Answers]
+    .findAll({
+        include: [{
+            model: db.models.Questions, include: [db.models.Answers]
         }]
     })
     .then(projects => {
         res.status(200).send(projects);
     })
 };
+
 const findProjectsById = (req, res) => {
     const projectId = req.params.projectId
     db.models.Projects
-    .findById(projectId, {include: [{
-        model: db.models.Questions, include: [db.models.Answers]
+    .findByPk(projectId, {
+        include: [{
+            model: db.models.Questions, include: [db.models.Answers]
         }]
     })
     .then(projects => {
         res.status(200).send(projects);
     })
 };
+
 const findFirstQuestion = (req, res) => {
     const projectId = req.params.projectId
     db.models.Projects
-    .findById(projectId, {include: [{
+    .findByPk(projectId, {include: [{
         model: db.models.Questions}], order: [
             [db.models.Questions, 'id', 'asc']]         
     })
@@ -48,23 +52,26 @@ const findFirstQuestion = (req, res) => {
         res.status(200).send(projects.questions[0]);
     })
 };
+
 const findQuestionById = (req, res) => {
     const questionId = req.params.questionId
     db.models.Questions
-    .findById(questionId, {
+    .findByPk(questionId, {
         include: [db.models.Answers]
     })
     .then(question  => {
-        question === null ? res.status(404).send([]) : res.send(question)
+        question === null ? res.status(404).send(["Jetzt bin ich überfragt!"]) : res.send(question)
     })
 };
+
 const findConclusionById = (req, res) => {
     const conclusionId = req.params.conclusionId;
-    db.models.Conclusions.findById(conclusionId)
+    db.models.Conclusions.findByPk(conclusionId)
     .then(conclusion => {
-        conclusion === null ? res.status(404).send([]) : res.send(conclusion)
+        conclusion === null ? res.status(404).send(["Jetzt bin ich überfragt!"]) : res.send(conclusion)
     })
 };
+
 const saveStatistics = (req,res) => {
     const {conclusion} = req.body;
     if (conclusion) {
@@ -88,7 +95,9 @@ const findConclusionByProject = (req, res) => {
     }
 })
     .then(projects => {
-        const tmp = projects.map((item, index) => {return item.title})
+        const tmp = projects.map((item, index) => {
+            return item.title
+        })
         res.status(200).send(tmp);
     })
 };
@@ -96,48 +105,83 @@ const findConclusionByProject = (req, res) => {
 const countItems = (req, res) => {
     const projectId = req.params.projectId;
     let total = 0;
-        db.models.Conclusions
-        .findAll({
-            where: {
+    let countresult = [];
+    let counter = 0;
+    db.models.Conclusions
+    .findAll({
+        where: {
             projectId : projectId
-            }   
-         })
-        .then(projects => {
-            const itemlist = projects.map((item, index) => {return item.title})
-            let countresult = [];
-            let counter = 0;
-
-            for (el of itemlist) {
-                db.models.Statistics.findAndCountAll({
-                    where: {
-                    item: el
-                    }
-                })
-                .then(cn => {
-                    countresult.push(cn)
-                    counter ++
-                    total = total + cn.count;
-                    if (counter === itemlist.length) {
-                        let resultToSend = [];
-                        for (index in itemlist) {
-                            if (countresult[index].count>0) {
-                                resultToSend.push({'count':Math.round(100/total*countresult[index].count), 'item':countresult[index].rows[0].item})
-                            }
-                        }
-                        function compare(a,b) {
-                            if (a.count > b.count)
-                              return -1;
-                            if (a.count < b.count)
-                              return 1;
-                            return 0;
-                          }
-                          resultToSend.sort(compare);
-                        res.status(200).send(resultToSend)
-                    }
-                })
-            }
+        }   
+    })
+    .then(projects => {
+        const itemlist = projects.map((item, index) => {
+            return item.title
         })
+        for (el of itemlist) {
+            db.models.Statistics.findAndCountAll({
+                where: {
+                    item: el
+                }
+            })
+            .then(cn => {
+                countresult.push(cn)
+                counter ++
+                total = total + cn.count;
+                if (counter === itemlist.length) {
+                    let resultToSend = [];
+                    for (index in itemlist) {
+                        if (countresult[index].count>0) {
+                            resultToSend.push({'count':Math.round(100/total*countresult[index].count), 'item':countresult[index].rows[0].item})
+                        }
+                    }
+                    function compare(a,b) {
+                        if (a.count > b.count) {
+                            return -1;
+                        }
+                        if (a.count < b.count) {
+                            return 1;
+                        }
+                    }
+                    resultToSend.sort(compare);
+                    res.status(200).send(resultToSend)
+                }
+            })
+        }
+    })
 };
+
+const customersById = (req, res) => {
+    const projectId = req.params.projectId;
+    let total = 0;
+    let counter = 0;
+    db.models.Conclusions
+    .findAll({
+        where: {
+            projectId : projectId
+        }
+    })
+    .then(projects => {
+        const itemlist = projects.map((item, index) => {
+            return item.title
+        })
+        for (el of itemlist) {
+            db.models.Statistics.findAndCountAll({
+                where: {
+                    item: el
+                }
+            })
+            .then(cn => {
+                counter ++
+                total = total + cn.count;
+                if (counter === itemlist.length) {
+                    res.status(200).send({'total':total})
+                }
+            })
+        }
+    })
+};
+
+
 
 module.exports = {
     homeRoute,
@@ -149,5 +193,6 @@ module.exports = {
     countItems,
     findConclusionByProject,
     findFirstQuestion,
-    landingpage
+    landingpage,
+    customersById
 }
--- backend/api/routes.js
+++ backend/api/routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const controllers = require('./controllers.js');
 
 router.get('/', controllers.homeRoute);
-router.get('/projects', controllers.findAllProjects);
+router.get('/projects', controllers.findAllProjects); //Unnötig
 router.get('/landingpage', controllers.landingpage);
 router.get('/projects/firstquestion/:projectId', controllers.findFirstQuestion);
-router.get('/projects/:projectId', controllers.findProjectsById);
+router.get('/projects/:projectId', controllers.findProjectsById); //Unnötig
 router.get('/questions/:questionId', controllers.findQuestionById);
 router.get('/conclusions/:conclusionId', controllers.findConclusionById);
-router.get('/conclusions/projects/:projectId', controllers.findConclusionByProject);
+router.get('/conclusions/projects/:projectId', controllers.findConclusionByProject); //Unnötig
 router.post('/statistics', controllers.saveStatistics);
 router.get('/statistics/:projectId', controllers.countItems);
-
+router.get('/customers/:projectId', controllers.customersById)
 module.exports = router;
